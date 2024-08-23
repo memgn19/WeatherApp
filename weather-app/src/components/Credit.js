4b659b2188081c1f155517ae2d109771990359b4
Elena <113547059+memgn19@users.noreply.github.com>
2024-08-23 18:57:51 +0200

    Weather app development

---
+++ weather-app/src/components/Credit.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://github.com/memgn19"
+        > Elena Moreno</a>, <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a>
+    </p>
+  </div>
+  );
+}
